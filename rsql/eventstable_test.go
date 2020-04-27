38842e3722893173e6bbe4ff3032f75b432b56bc
Vaughan Muller <vaughan@luno.com>
2020-04-27 20:04:13 +0200

    reflex: Add github action build config
    Reviewers: #backend, #pod-shield
    
    Differential Revision: https://phabricator.corp.luno.com/D52170

--- rsql/eventstable_test.go
+++ rsql/eventstable_test.go
@@ -487,5 +487,5 @@ func TestInMemNotifier(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, int64(1), e.ForeignIDInt())
 	require.True(t, time.Since(t0) > lag, "want: %s\ngot: %s", lag, time.Since(t0))
-	require.True(t, time.Since(t0) < 10*time.Second, time.Since(t0))
+	require.True(t, time.Since(t0) < 5*time.Second, time.Since(t0))
 }
