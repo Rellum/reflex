d4a0cc9029e7966c7461ee1759b2605c55ad309e
Vaughan Muller <vaughan@luno.com>
2020-04-27 19:39:29 +0200

    Review fix

--- rsql/eventstable_test.go
+++ rsql/eventstable_test.go
@@ -486,6 +486,6 @@ func TestInMemNotifier(t *testing.T) {
 	e, err := sc.Recv()
 	require.NoError(t, err)
 	require.Equal(t, int64(1), e.ForeignIDInt())
-	require.True(t, time.Since(t0) > lag)
-	require.True(t, time.Since(t0) < 2*lag)
+	require.True(t, time.Since(t0) > lag, "want: %s\ngot: %s", lag, time.Since(t0))
+	require.True(t, time.Since(t0) < 3*lag, "want: %s\ngot: %s", 3*lag, time.Since(t0))
 }
